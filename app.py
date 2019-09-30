544b190b94b16fc0a298ce8368e374dc4ba93001
AldahirMartinez <aldahir.martinez9197@gmail.com>
2019-09-30 09:19:24 -0400

    update add.py

--- app.py
+++ app.py
@@ -32,6 +32,18 @@ def index():
     else:
         tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html', tasks=tasks)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was a problem deleting'
+
+
 if __name__=="__main__":
     app.run(debug=True)
 
